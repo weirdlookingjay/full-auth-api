a96b312928034ec858a803d923aedc5984057346
Joe Clark <weirdlookingjay@live.com>
2025-05-19 10:24:54 -0400

    Frontend: added Verication route

--- frontend/app/activation/[uid]/[token]/page.tsx
+++ frontend/app/activation/[uid]/[token]/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client"
 
-const Page = () => {
-    return (
-        <main>
-            <h1>Activation Page</h1>
-        </main>
-    )
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import { useActivationMutation } from "@/redux/features/authApiSlice"
+import { toast } from "react-toastify"
+
+interface Props {
+    params: {
+        uid: string
+        token: string
+    }
 }
 
-export default Page
+export default function Page({ params }: Props) {
+    const router = useRouter()
+
+    const [activation] = useActivationMutation()
+
+    useEffect(() => {
+        const { uid, token } = params
+        activation({ uid, token }).unwrap().then(() => {
+            toast.success("Account activated successfully")
+        }).catch(() => {
+            toast.error("Failed to activate account")
+        }).finally(() => {
+            router.push("/auth/login")
+        })
+    }, [])
+
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Activating your account...</h1>
+            </div>
+        </div>
+    )
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Provider>
           <Setup />
           <Navbar />
-          <div>{children}</div>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8">{children}</div>
           <Footer />
         </Provider>
       </body>
